8ef0c4854f351b4822b24efdfdab25ed9eb6523d
Victor Nin <mrnin33@Victors-MacBook-Air.local>
2023-05-02 16:05:58 -0400

    Run GUI

---
+++ EnigmaGUI.java
@@ -0,0 +1,21 @@
+public class EnigmaGUI {
+
+    public static void main(String args[]){
+        
+       /*  JButton Decrypt = new JButton("Calculate!");
+
+        Decrypt.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String message = Input.getText();
+                String decryptedmessage = run(message);
+                Output.setText(decryptedmessage);
+            }
+        });
+*/
+        EnigmaFrame u = new EnigmaFrame();
+        u.setVisible(true);
+
+
+
+    }
+}
